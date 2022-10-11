8a9e569bb01e9f9ca18c9128c1d0190f8b29906e
Juche <azhucheng1@qq.com>
2022-10-11 14:14:49 +0800

    代码报错 Universe.new is not a constructor

--- www/index.js
+++ www/index.js
@@ -1,3 +1,14 @@
-import * as wasm from 'ju-rust-wasm';
+// import * as wasm from 'ju-rust-wasm';
+import { greet, Universe } from 'ju-rust-wasm';
 
-wasm.greet('Juching & Juche');
+greet('Juching & Juche');
+
+const pre = document.getElementById('game-of-life-canvas');
+const universe = new Universe.new();
+console.log(`🚀 ~ universe`, universe);
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+  requestAnimationFrame(renderLoop);
+};
+renderLoop();
