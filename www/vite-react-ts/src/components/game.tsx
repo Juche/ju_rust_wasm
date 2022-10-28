98a1a2dfbcc17bbcfbe02ae38942d2397582593b
Juching <azhucheng1@qq.com>
2022-10-28 10:31:58 +0800

    passing props & event

--- www/vite-react-ts/src/components/game.tsx
+++ www/vite-react-ts/src/components/game.tsx
@@ -7,13 +7,17 @@ import '../styles/game.css';
 
 class Square extends React.Component {
   render() {
-    return <button className='square'>{this.props.value}</button>;
+    return (
+      <button className='square' onClick={() => console.log(this.props.value)}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
-  renderSquare() {
-    return <Square></Square>;
+  renderSquare(i) {
+    return <Square value={i}></Square>;
   }
 
   render() {
@@ -22,19 +26,19 @@ class Board extends React.Component {
       <div className='boader-ctn'>
         <div className='status'>{status}</div>
         <div className='boader-row'>
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className='boader-row'>
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className='boader-row'>
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
