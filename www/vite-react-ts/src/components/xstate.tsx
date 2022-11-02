441e398f644115e21805dfcafa17a2161e83684d
Juching <azhucheng1@qq.com>
2022-11-02 16:04:50 +0800

    update state

--- www/vite-react-ts/src/components/xstate.tsx
+++ www/vite-react-ts/src/components/xstate.tsx
@@ -3,19 +3,32 @@ import { useMachine } from '@xstate/react';
 
 const promiseMachine = createMachine({
   id: 'promise',
-  initial: 'pending',
+  initial: 'idle',
   states: {
+    idle: {
+      on: {
+        NEW: 'pending',
+      },
+    },
     pending: {
       on: {
-        RESOLVE: { target: 'resolved' },
-        REJECT: { target: 'rejected' },
+        RESOLVE: 'resolved',
+        REJECT: 'rejected',
+        RESET: 'idle',
       },
     },
     resolved: {
-      type: 'final',
+      // type: 'final',
+      on: {
+        RESET: 'idle',
+      },
     },
     rejected: {
-      type: 'final',
+      // type: 'final',
+      on: {
+        RETRY: 'pending',
+        RESET: 'idle',
+      },
     },
   },
 });
@@ -25,14 +38,25 @@ export function PromiseState() {
 
   return (
     <div className='promise-machine'>
+      Promise State:
+      {state.matches('idle') && <p>Idle</p>}
       {state.matches('pending') && <p>Loading...</p>}
       {state.matches('resolved') && <p>Promise Resolved</p>}
       {state.matches('rejected') && <p>Promise Rejected</p>}
+      <button className='new' onClick={() => send('NEW')}>
+        New Promise
+      </button>
       <button className='resolve' onClick={() => send('RESOLVE')}>
         Resolve
       </button>
       <button className='reject' onClick={() => send('REJECT')}>
         Reject
+      </button>
+      <button className='retry' onClick={() => send('RETRY')}>
+        Retry
+      </button>
+      <button className='reset' onClick={() => send('RESET')}>
+        Reset
       </button>
     </div>
   );
