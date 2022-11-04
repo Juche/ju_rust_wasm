3f8d4d747b28b93ae5e7e2aa2793ddaf88b04432
Juching <azhucheng1@qq.com>
2022-11-04 12:15:17 +0800

    localforage 优化传参 & 方法调用

--- www/vite-react-ts/src/components/localforage.tsx
+++ www/vite-react-ts/src/components/localforage.tsx
@@ -1,14 +1,39 @@
-import { clear, getItem, removeItem, setItem } from 'localforage';
+import localforage, { clear, getItem, removeItem, setItem } from 'localforage';
 
 const key = 'key1';
 const val = 'val1';
 
-type HandleDataParams = [fn: Function, key?: string, val?: any];
+// type forageFn =
+//   | 'getItem'
+//   | 'setItem'
+//   | 'removeItem'
+//   | 'clear'
+//   | 'length'
+//   | 'key'
+//   | 'keys'
+//   | 'iterate';
+type HandleDataParams = [fn: string, key?: string, val?: any];
 
 function handleData(...[fn, key, val]: HandleDataParams) {
   (async (): Promise<void> => {
     try {
-      await fn(key, val);
+      // const isGet = fn.toString().startsWith('function getItem');
+      // console.log(`🚀 ~ fn.toString()`, fn.toString());
+      switch (fn) {
+        case 'getItem':
+          const _val = await localforage[fn](key!);
+          console.log(`🚀 ~ _val`, _val);
+          break;
+        case 'setItem':
+          await localforage[fn](key!, val);
+          break;
+        case 'removeItem':
+          await localforage[fn](key!);
+          break;
+        case 'clear':
+          await localforage[fn]();
+          break;
+      }
     } catch (error) {
       console.log(`🚀 ~ handleData ~ error`, error);
     }
@@ -20,16 +45,16 @@ export function Storage() {
     <div className='ctn'>
       <input value={key} />
       <input value={val} />
-      <button className='set' onClick={() => handleData(setItem, key, val)}>
+      <button className='set' onClick={() => handleData('setItem', key, val)}>
         存
       </button>
-      <button className='get' onClick={() => handleData(getItem, key)}>
+      <button className='get' onClick={() => handleData('getItem', key)}>
         取
       </button>
-      <button className='remove' onClick={() => handleData(removeItem, key)}>
+      <button className='remove' onClick={() => handleData('removeItem', key)}>
         删
       </button>
-      <button className='clean' onClick={() => handleData(clear)}>
+      <button className='clean' onClick={() => handleData('clear')}>
         空
       </button>
     </div>
